9cfb7bd7da614e12ba7e97048a5cad0ccfe777f5
Abhinav <abhinavpneupane@gmail.com>
2026-05-29 15:34:59 -0500

    Manually implemeted huggingface's Universal Assisted Generation using llama3.2 3b and gpt2-large

--- test4.py
+++ test4.py
@@ -45,16 +45,78 @@ def normal_inference(model, tokenizer, prompt, max_new_tokens=30, device="cuda")
         distributions.append(prob_dist)
         token_probs.append((tokenizer.decode([token_id]), prob))
 
-    return text, new_tokens, token_probs, distributions
+    return text
 
 
-def speculative_decoding(model, assistant_model, tokenizer, assistant_tokenizer, prompt, max_new_tokens=30, device="cuda"):
-    inputs = tokenizer(prompt, return_tensors="pt").to(device)
-    outputs = large_model.generate(**inputs, assistant_model=assistant_model, tokenizer=tokenizer, assistant_tokenizer=assistant_tokenizer,pad_token_id=large_tokenizer.eos_token_id, max_new_tokens=30, return_dict_in_generate=True)
+def speculative_decoding(prompt, target_model, target_tokenizer, assistant_model, assistant_tokenizer, device="cuda", max_new_tokens=30):
+    current_text = prompt
+    generated_tokens = 0
+    gamma = 5
 
-    res = large_tokenizer.batch_decode(outputs.sequences[0], skip_special_tokens=True)
+    accepted_texts = []
 
-    return res
+    while generated_tokens < max_new_tokens:
+        # Draft generation
+        ast_inputs = assistant_tokenizer(current_text, return_tensors="pt").to(device)
+
+        # Generate gamma tokens
+        ast_draft_outputs = assistant_model.generate(
+            ast_inputs["input_ids"],
+            max_new_tokens=gamma,
+            attention_mask= ast_inputs["attention_mask"],  
+            pad_token_id=assistant_tokenizer.eos_token_id, 
+        )
+
+        # Extract the new draft tokens
+        prompt_len = ast_inputs.input_ids.shape[1]
+        draft_tokens = ast_draft_outputs[0][prompt_len:]
+
+
+        # Decode draft tokens to plain text
+        draft_text = assistant_tokenizer.decode(draft_tokens, skip_special_tokens=True)
+        proposed_text = current_text + draft_text
+
+
+        # Tokenize the decoded tokens through target model
+        tgt_inputs = target_tokenizer(proposed_text, return_tensors="pt").to(device)
+        tgt_prompt_ids = target_tokenizer(current_text, return_tensors="pt").input_ids
+
+        tgt_prompt_len = tgt_prompt_ids.shape[1]
+        proposed_tgt_tokens = tgt_inputs.input_ids[0][tgt_prompt_len:]
+
+        with torch.no_grad():
+            outputs = target_model(tgt_inputs["input_ids"])
+            logits = outputs.logits[0]
+        
+        accepted_tokens = []
+        
+        # Acceptance / Rejection
+        for i in range(len(proposed_tgt_tokens)):
+            step_logits = logits[tgt_prompt_len + i - 1]
+
+            target_expected_token = torch.argmax(step_logits).item()
+            proposed_token = proposed_tgt_tokens[i].item()
+
+            if target_expected_token == proposed_token:
+                accepted_tokens.append(target_expected_token)
+            else:
+                accepted_tokens.append(target_expected_token)
+                break
+            
+
+        # Decode only verified + corrected tokens
+        accepted_ids_tensor = torch.tensor([accepted_tokens])
+        accepted_text = target_tokenizer.decode(accepted_ids_tensor[0], skip_special_tokens=True)
+
+        current_text += accepted_text
+        generated_tokens += len(accepted_tokens)
+
+        accepted_texts.append(accepted_text)
+
+        if target_tokenizer.eos_token_id in accepted_tokens:
+            break
+    
+    return current_text, accepted_texts, len(accepted_texts)
 
 
 def measure_latency(func, *args, **kwargs):
@@ -78,6 +140,7 @@ def measure_latency(func, *args, **kwargs):
 
 prompt = "Once upon a time, there was a little girl named Alice who lived in a small village."
 
+
 # Measure Normal Inference
 normal_latency, normal_outputs = measure_latency(
     normal_inference, 
@@ -91,22 +154,15 @@ normal_latency, normal_outputs = measure_latency(
 # Measure Speculative Decoding
 sd_latency, sd_outputs = measure_latency(
     speculative_decoding, 
-    large_model, 
-    small_model,
-    large_tokenizer, 
-    small_tokenizer, 
     prompt, 
+    large_model,
+    large_tokenizer, 
+    small_model, 
+    small_tokenizer,
+    device, 
     max_new_tokens, 
-    device
 )
 
-print(normal_outputs[0], normal_latency)
-print(sd_outputs, sd_latency)
+print(normal_outputs, normal_latency)
+print(sd_outputs[0], sd_outputs[2], sd_latency)
 
-
-# Results
-'''
-Once upon a time, there was a little girl named Alice who lived in a small village. One day, Alice was playing in the meadow when she saw a rabbit running away from her. Alice chased the rabbit and caught it, but it 1.2139147724956274
-['Once upon a time, there was a little girl named Alice who lived in a small village. She was a very kind and gentle girl, and she loved to help others. One day, she was walking through the forest when she saw a little'] 1.7380648087710142
-
-'''
